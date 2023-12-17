0103ddda4e1484bbd5cf694e03c05161f6c42896
Roberto Branco <bebeto.fb@hotmail.com>
2023-12-17 19:15:22 +0100

    "Pong: adds constants file"

---
+++ Pong/constants.py
@@ -0,0 +1,19 @@
+import pygame as pg
+
+# IMAGES
+BACKGROUND = pg.image.load('midia/backgroung.png')
+BACKGROUND_2 = pg.image.load('midia/back2.png')
+
+# COLORS
+DARK_RED = (120, 0, 0)
+LIGHT_RED = (255, 0, 0)
+DARK_YELLOW = (140, 140, 0)
+LIGHT_YELLOW = (220, 220, 0)
+DARK_GREEN = (0, 130, 0)
+LIGHT_GREEN = (0, 255, 0)
+DARK_BLUE = (0, 0, 120)
+LIGHT_BLUE = (0, 0, 255)
+PINK = (240, 0, 130)
+PURPLE = (51, 0, 102)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
